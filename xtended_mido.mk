5ec7cd0ac225db7c5976978e11b4df9318cee13d
GHOST <jijinraj401@gmail.com>
2020-04-11 11:39:36 +0000

    wtf

--- xtended_mido.mk
+++ xtended_mido.mk
@@ -36,7 +36,7 @@ ro.xtended.maintainer ="GH0ST"
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := xtended/_mido
+PRODUCT_NAME := xtended_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
