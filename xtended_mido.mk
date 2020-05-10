72f99ec2cc50de7ddcfa6240c521cb44b2d25436
GHOST <jijinraj401@gmail>
2020-05-10 20:19:43 +0530

    Enforced + Fixed Maintainer Name

--- xtended_mido.mk
+++ xtended_mido.mk
@@ -30,6 +30,10 @@ TARGET_BOOT_ANIMATION_RES := 1080
 # Gapps
 TARGET_GAPPS_ARCH := arm64
 
+# Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+        ro.xtended.maintainer=☭GH05T☭
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
 PRODUCT_NAME := xtended_mido
