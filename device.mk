00770bbe6ba1baa2bf74dfb6c535766b1566f599
GHOST <jijinraj401@gmail>
2020-05-11 09:56:58 +0530

    Call 4.9 Vendors Insted of mido vendors

--- device.mk
+++ device.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-$(call inherit-product, vendor/xiaomi/mido/mido-vendor.mk)
+$(call inherit-product, vendor/xiaomi/msm8953/mido/mido-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Setup dalvik vm configs
