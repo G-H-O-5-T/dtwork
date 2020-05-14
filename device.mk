9e3fc7409fb4cda01802b82e74849c5bc86541ea
ZeeLog <Danjel.Zammit@gmail.com>
2020-05-14 11:26:15 +0200

    mido: Add GraphicBuffer shim for camera
    * Address errors related to cannot locate symbol
      "_ZN7android13GraphicBufferC1EjjijjP13native_handleb"
      referenced by "/vendor/lib/libmmcamera_ppeiscore.so"...
    
    Change-Id: I73a6e85de78f3f35cb421a52666f8ffa3dbd0898

--- device.mk
+++ device.mk
@@ -129,7 +129,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
-    libmm-qcamera
+    libmm-qcamera \
+    libshim_camera \
+    Snap
 
 # Consumerir
 PRODUCT_PACKAGES += \
