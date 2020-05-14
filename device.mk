e1177a1d017a3c8d17ee54c48a2b672d6ca7833c
luca020400 <luca.stefani.ge1@gmail.com>
2020-05-14 12:01:26 +0530

    mido: Enable updatable APEX

--- device.mk
+++ device.mk
@@ -16,6 +16,7 @@
 
 $(call inherit-product, vendor/xiaomi/mido/mido-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
